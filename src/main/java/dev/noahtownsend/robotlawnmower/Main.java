a3eb63988d01b2f8527a5d4ce4c7e5c65b1341ea
Noah Townsend <noah@noahTownsend.com>
2022-03-28 09:48:01 -0400

    add run script

--- src/main/java/dev/noahtownsend/robotlawnmower/Main.java
+++ src/main/java/dev/noahtownsend/robotlawnmower/Main.java
@@ -26,27 +26,11 @@ public class Main {
 
 // lets invoke some changes on the digital output
         output.state(DigitalState.HIGH)
-                .state(DigitalState.LOW)
-                .state(DigitalState.HIGH)
                 .state(DigitalState.LOW);
-
-// lets toggle the digital output state a few times
-        output.toggle()
-                .toggle()
-                .toggle();
-
-// another friendly method of setting output state
-        output.high()
-                .low();
 
 // lets read the digital output state
         System.out.print("CURRENT DIGITAL OUTPUT [" + output + "] STATE IS [");
         System.out.println(output.state() + "]");
-
-// pulse to HIGH state for 3 seconds
-        System.out.println("PULSING OUTPUT STATE TO HIGH FOR 3 SECONDS");
-        output.pulse(3, TimeUnit.SECONDS, DigitalState.HIGH);
-        System.out.println("PULSING OUTPUT STATE COMPLETE");
 
 
 // shutdown Pi4J
