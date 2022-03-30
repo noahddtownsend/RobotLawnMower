a8ba12d686d925b6453289b5e11c7fa48d833c7f
Noah Townsend <noah@noahTownsend.com>
2022-03-29 21:54:57 -0400

    Blink LED

--- src/main/java/dev/noahtownsend/robotlawnmower/Main.java
+++ src/main/java/dev/noahtownsend/robotlawnmower/Main.java
@@ -4,45 +4,94 @@ import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.util.Console;
 
 public class Main {
-    private static int DIGITAL_OUTPUT_PIN = 22;
+    private static final int PIN_BUTTON = 24; // PIN 18 = BCM 24
+    private static final int PIN_LED = 22; // PIN 15 = BCM 22
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello world");
+    private static int pressCount = 0;
 
-        blink(DIGITAL_OUTPUT_PIN);
-    }
+    /**
+     * This application blinks a led and counts the number the button is pressed. The blink speed increases with each
+     * button press, and after 5 presses the application finishes.
+     *
+     * @param args an array of {@link java.lang.String} objects.
+     * @throws java.lang.Exception if any.
+     */
+    public static void main(String[] args) throws Exception {
+        // Create Pi4J console wrapper/helper
+        // (This is a utility class to abstract some of the boilerplate stdin/stdout code)
+        final var console = new Console();
 
-    public static void blink(int ledNum) throws InterruptedException {
-        Context pi4j = Pi4J.newAutoContext();
-        System.out.println("Hi there!");
+        // Print program title/header
+        console.title("<-- The Pi4J Project -->", "Minimal Example project");
 
+        // ************************************************************
+        //
+        // WELCOME TO Pi4J:
+        //
+        // Here we will use this getting started example to
+        // demonstrate the basic fundamentals of the Pi4J library.
+        //
+        // This example is to introduce you to the boilerplate
+        // logic and concepts required for all applications using
+        // the Pi4J library.  This example will do use some basic I/O.
+        // Check the pi4j-examples project to learn about all the I/O
+        // functions of Pi4J.
+        //
+        // ************************************************************
 
-        // create a digital output instance using the default digital output provider
-        DigitalOutput output = pi4j.create(DigitalOutput.newConfigBuilder(pi4j)
+        // ------------------------------------------------------------
+        // Initialize the Pi4J Runtime Context
+        // ------------------------------------------------------------
+        // Before you can use Pi4J you must initialize a new runtime
+        // context.
+        //
+        // The 'Pi4J' static class includes a few helper context
+        // creators for the most common use cases.  The 'newAutoContext()'
+        // method will automatically load all available Pi4J
+        // extensions found in the application's classpath which
+        // may include 'Platforms' and 'I/O Providers'
+        var pi4j = Pi4J.newAutoContext();
+
+        // Here we will create I/O interfaces for a (GPIO) digital output
+        // and input pin. We define the 'provider' to use PiGpio to control
+        // the GPIO.
+        var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
                 .id("led")
                 .name("LED Flasher")
-                .address(ledNum)
+                .address(PIN_LED)
                 .shutdown(DigitalState.LOW)
                 .initial(DigitalState.LOW)
-                .provider("pigpio-digital-output"));
-
-// setup a digital output listener to listen for any state changes on the digital output
-        output.addListener(System.out::println);
-
-// lets invoke some changes on the digital output
-        output.low();
-        Thread.sleep(5000);
-        output.high();
-        Thread.sleep(5000);
-
-// lets read the digital output state
-        System.out.print("CURRENT DIGITAL OUTPUT [" + output + "] STATE IS [");
-        System.out.println(output.state() + "]");
+                .provider("pigpio-digital-output");
+        var led = pi4j.create(ledConfig);
 
 
-// shutdown Pi4J
+        while (pressCount < 5) {
+            ++pressCount;
+            if (led.equals(DigitalState.HIGH)) {
+                console.println("LED low");
+                led.low();
+            } else {
+                console.println("LED high");
+                led.high();
+            }
+            Thread.sleep(500 / (pressCount + 1));
+        }
+
+        // ------------------------------------------------------------
+        // Terminate the Pi4J library
+        // ------------------------------------------------------------
+        // We we are all done and want to exit our application, we must
+        // call the 'shutdown()' function on the Pi4J static helper class.
+        // This will ensure that all I/O instances are properly shutdown,
+        // released by the the system and shutdown in the appropriate
+        // manner. Terminate will also ensure that any background
+        // threads/processes are cleanly shutdown and any used memory
+        // is returned to the system.
+
+        // Shutdown Pi4J
         pi4j.shutdown();
     }
 }
