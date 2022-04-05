5fab54033ca0761e0bc99420545ba6e76301a192
Noah Townsend <noah@noahTownsend.com>
2022-04-04 22:57:35 -0400

    Test Kotlin

---
+++ src/main/java/dev/noahtownsend/robotlawnmower/KotlinTest.kt
@@ -0,0 +1,11 @@
+package dev.noahtownsend.robotlawnmower
+
+import java.sql.DriverManager.println
+
+class KotlinTest {
+    companion object {
+        fun hello(){
+            println("Hello World!")
+        }
+    }
+}
